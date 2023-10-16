62eb538215b26df98c7340e465278b237ea2d8de
Ezz Alhamzi <aaalhamzi57@gmail.com>
2023-10-16 13:51:00 +0300

    A class to represent a student with ID, name, and age.

---
+++ Student Management.py
@@ -0,0 +1,6 @@
+# A class to represent a student with ID, name, and age.
+class Student:
+    def __init__(self, ID, name, age):
+        self.ID = ID
+        self.name = name
+        self.age = age
